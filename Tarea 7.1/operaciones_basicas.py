531477fd04ea5c4b8f6b325c6f53da690cd81570
Joseph <113220181+Cachimbot@users.noreply.github.com>
2023-01-08 18:24:08 -0500

    Ejercicio 7.1

---
+++ Tarea 7.1/operaciones_basicas.py
@@ -0,0 +1,18 @@
+
+
+global x
+global y
+x = float(input("Digite su primer numero: "))
+y = float(input("Digite su segundo numero: "))
+
+def suma():
+    return f"La suma es: {x + y}"
+
+def resta():
+    return f"La resta es : {x - y}"
+
+def multiplicacion():
+    return f"La multiplicacion es: {x * y}"
+
+def division():
+    return f"La division es: {x / y}"
